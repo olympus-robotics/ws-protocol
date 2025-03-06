1b1292e8949a7393ba0a00f70f00cd2be011d59b
Thomas Schneider <thomas.schneider@voliro.ch>
2025-03-06 17:29:04 +0100

    bump

--- cpp/foxglove-websocket/include/foxglove/websocket/websocket_server.hpp
+++ cpp/foxglove-websocket/include/foxglove/websocket/websocket_server.hpp
@@ -259,6 +259,7 @@ inline Server<ServerConfiguration>::Server(std::string name, LogCallback logger,
   _server.set_validate_handler(std::bind(&Server::validateConnection, this, std::placeholders::_1));
   _server.set_open_handler(std::bind(&Server::handleConnectionOpened, this, std::placeholders::_1));
   _server.set_ping_handler([](auto, auto) {
+    _server.get_elog().write(RECOVERABLE, "PIIING");
     return true;
   });
   _server.set_close_handler(
