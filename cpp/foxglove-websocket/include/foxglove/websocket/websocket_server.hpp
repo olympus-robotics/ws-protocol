1608b75ec0c1f2ee50c051a624962b9a9afb48d5
Thomas Schneider <thomas.schneider@voliro.ch>
2025-03-07 09:25:08 +0100

    bump

--- cpp/foxglove-websocket/include/foxglove/websocket/websocket_server.hpp
+++ cpp/foxglove-websocket/include/foxglove/websocket/websocket_server.hpp
@@ -8,6 +8,7 @@
 #include <chrono>
 #include <cstdint>
 #include <functional>
+#include <iostream>
 #include <map>
 #include <memory>
 #include <optional>
@@ -258,8 +259,9 @@ inline Server<ServerConfiguration>::Server(std::string name, LogCallback logger,
   this->setupTlsHandler();
   _server.set_validate_handler(std::bind(&Server::validateConnection, this, std::placeholders::_1));
   _server.set_open_handler(std::bind(&Server::handleConnectionOpened, this, std::placeholders::_1));
-  _server.set_ping_handler([this](auto, auto) {
+  _server.set_ping_handler([this](auto, auto paylad) {
     _server.get_elog().write(RECOVERABLE, "PIIING");
+    std::cout << "ping: " << paylad << std::endl;
     return true;
   });
   _server.set_close_handler(
